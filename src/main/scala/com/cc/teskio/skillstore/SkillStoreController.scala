d60b66fd5676ae191730cd973129ee937c13fdf4
Jan Kurella <jan.kurella@meteogroup.com>
2020-12-17 11:07:23 +0100

    adding skills to in-memory store

--- src/main/scala/com/cc/teskio/skillstore/SkillStoreController.scala
+++ src/main/scala/com/cc/teskio/skillstore/SkillStoreController.scala
@@ -2,16 +2,26 @@ package com.cc.teskio.skillstore
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation._
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.{RequestMapping, _}
 
 
 @Autowired
+@Service
 @RestController
 class SkillStoreController {
 
+  /** mapping source ⇒ id ⇒ skills */
+  var skillStore: Map[String, Map[String, Set[String]]] = Map();
+
   @RequestMapping(path = Array("/store"), method = Array(RequestMethod.GET))
-  def getSkills(): ResponseEntity[String]  ={
-    ResponseEntity.ok("""{"result":"ok"}""")
+  def getSkills(): ResponseEntity[String] = {
+    ResponseEntity.ok(s"""{"result":"ok", "count":${skillStore.size}""")
+  }
+
+  /** this will be later callled from the POST controller, once this is a single service */
+  def addSkills(source: String, id: String, skills: Set[String]): Unit = {
+    skillStore = skillStore + (source → (skillStore.getOrElse(source, Map()) + (id → skills)))
   }
 
 }
---
+++ src/test/scala/com/cc/teskio/skillstore/SkillStoreControllerTest.scala
@@ -0,0 +1,33 @@
+package com.cc.teskio.skillstore
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ */
+class SkillStoreControllerTest extends FunSuite with Matchers{
+
+  test("testAddSkills") {
+    val storeController = new SkillStoreController
+    storeController.skillStore should have size 0
+    storeController.addSkills("github", "jku", Set("java"))
+    storeController.skillStore.get("github").flatMap(_.get("jku")).get shouldEqual(Set("java"))
+  }
+
+  test("testUpdateSkills") {
+    val storeController = new SkillStoreController
+    storeController.skillStore should have size 0
+    storeController.addSkills("github", "jku", Set("java"))
+    storeController.addSkills("github", "jku", Set("scala"))
+    storeController.skillStore.get("github").flatMap(_.get("jku")).get shouldEqual(Set("scala"))
+  }
+
+  test("testKeepOtherSkills") {
+    val storeController = new SkillStoreController
+    storeController.skillStore should have size 0
+    storeController.addSkills("github", "jku", Set("scala"))
+    storeController.addSkills("github", "eric", Set("java"))
+    storeController.addSkills("stackoverflow", "eric", Set("java"))
+    storeController.skillStore.get("github").flatMap(_.get("jku")).get shouldEqual(Set("scala"))
+  }
+
+}
